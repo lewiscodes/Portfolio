dbdaa8b0e615b5c6d06c6eeb6dce0050d19d90af
Lewis Turner <lewis.turner@lewiss-air.home>
2019-02-21 15:34:46 +0000

    inital work on work carousel

--- js/index.js
+++ js/index.js
@@ -1,6 +1,56 @@
 var $ = window.$
 
-$('.name').on('click tap', function () {
-  $('html, body').animate({ scrollTop: 0 }, 'fast')
-  return false
+$(document).ready(function () {
+  $('.name').on('click tap', function () {
+    $('html, body').animate({ scrollTop: 0 }, 'fast')
+    return false
+  })
+
+  $('.slide').css({width: window.innerWidth})
+  var slideCount = $('.slider ul li').length
+  var slideWidth = $('.slider ul li').width()
+  var slideHeight = $('.slider ul li').height()
+  var sliderFullWidth = (slideCount * slideWidth)
+
+  $('.slider').css({ width: slideWidth, height: slideHeight })
+  $('.slider ul').css({ width: sliderFullWidth, marginLeft: -slideWidth })
+  $('.slider ul li:last-child').prependTo('.slider ul')
+
+  $(window).on('resize', function () {
+    $('.slide').css({width: window.innerWidth})
+    slideCount = $('.slider ul li').length
+    slideWidth = $('.slider ul li').width()
+    slideHeight = $('.slider ul li').height()
+    sliderFullWidth = (slideCount * slideWidth)
+
+    $('.slider').css({ width: slideWidth, height: slideHeight })
+    $('.slider ul').css({ width: sliderFullWidth, marginLeft: -slideWidth })
+    $('.slider ul li:last-child').prependTo('.slider ul')
+  })
+
+  $('a.control_prev').click(function () {
+    moveLeft(slideWidth)
+  })
+
+  $('a.control_next').click(function () {
+    moveRight(slideWidth)
+  })
 })
+
+function moveRight (slideWidth) {
+  $('.slider ul').animate({
+    left: -slideWidth
+  }, 200, function () {
+    $('.slider ul li:first-child').appendTo('.slider ul')
+    $('.slider ul').css('left', '')
+  })
+}
+
+function moveLeft (slideWidth) {
+  $('.slider ul').animate({
+    left: +slideWidth
+  }, 200, function () {
+    $('.slider ul li:last-child').prependTo('.slider ul')
+    $('.slider ul').css('left', '')
+  })
+}
