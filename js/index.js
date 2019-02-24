d9a63dda5cd0323d41818368aabf53d89b6607d4
Lewis Turner <lewis.turner@lewiss-mbp.home>
2019-02-24 10:10:54 +0000

    added content, styled and animated carousel; fixed a few little bug and added scrolling

--- js/index.js
+++ js/index.js
@@ -1,18 +1,31 @@
 var $ = window.$
+var selectedSlide = 1
+var slideCount = null
+var slideWidth = null
+var slideHeight = null
+var sliderFullWidth = null
 
 $(document).ready(function () {
   $('.name').on('click tap', function () {
-    $('html, body').animate({ scrollTop: 0 }, 'fast')
+    $('html, body').animate({ scrollTop: 0 }, 750)
     return false
   })
 
-  $('.slide').css({width: window.innerWidth})
-  var slideCount = $('.slider ul li').length
-  var slideWidth = $('.slider ul li').width()
-  var slideHeight = $('.slider ul li').height()
-  var sliderFullWidth = (slideCount * slideWidth)
+  $('.work').on('click tap', function () {
+    var target = $('.section.work').offset()
+    target.top = target.top - 50
+    
+    $('html, body').animate({
+      scrollTop: target.top
+    }, 750);
+  })
 
-  $('.slider').css({ width: slideWidth, height: slideHeight })
+  $('.slide').css({width: window.innerWidth})
+  slideCount = $('.slider ul li').length
+  slideWidth = $('.slider ul li').width()
+  sliderFullWidth = (slideCount * slideWidth)
+
+  $('.slider').css({ width: slideWidth })
   $('.slider ul').css({ width: sliderFullWidth, marginLeft: -slideWidth })
   $('.slider ul li:last-child').prependTo('.slider ul')
 
@@ -20,7 +33,6 @@ $(document).ready(function () {
     $('.slide').css({width: window.innerWidth})
     slideCount = $('.slider ul li').length
     slideWidth = $('.slider ul li').width()
-    slideHeight = $('.slider ul li').height()
     sliderFullWidth = (slideCount * slideWidth)
 
     $('.slider').css({ width: slideWidth, height: slideHeight })
@@ -30,17 +42,38 @@ $(document).ready(function () {
 
   $('a.control_prev').click(function () {
     moveLeft(slideWidth)
+    setDot('prev')
   })
 
   $('a.control_next').click(function () {
     moveRight(slideWidth)
+    setDot('next')
   })
 })
+
+function setDot (direction) {
+  if (direction === 'next') {
+    if (selectedSlide === $('.slide').length) {
+      selectedSlide = 1
+    } else {
+      selectedSlide++
+    }
+  } else {
+    if (selectedSlide === 1) {
+      selectedSlide = $('.slide').length
+    } else {
+      selectedSlide--
+    }
+  }
+
+  $('.dot.selected').removeClass('selected')
+  $($('.dot')[selectedSlide - 1]).addClass('selected')
+}
 
 function moveRight (slideWidth) {
   $('.slider ul').animate({
     left: -slideWidth
-  }, 200, function () {
+  }, 750, function () {
     $('.slider ul li:first-child').appendTo('.slider ul')
     $('.slider ul').css('left', '')
   })
@@ -49,7 +82,7 @@ function moveRight (slideWidth) {
 function moveLeft (slideWidth) {
   $('.slider ul').animate({
     left: +slideWidth
-  }, 200, function () {
+  }, 750, function () {
     $('.slider ul li:last-child').prependTo('.slider ul')
     $('.slider ul').css('left', '')
   })
